6a20d3d8225ae989e3b1aa33837fb9c625763516
elmachotroso <me@andreivictor.net>
2021-09-22 22:02:33 +0800

    Full implementation.

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ function compute()
 {
     // retrieve the value of the principal element and validate
     // the value should be positive and not empty.
-    var principal = document.getElementById("principal").value;
+    var principalObj = document.getElementById("principal");
+    var principal = principalObj.value;
     if(principal <= 0)
     {
         alert("Enter a positive number.");
-        principal.focus();
+        principalObj.focus();
         return;
     }
 
@@ -22,15 +23,19 @@ function compute()
     var interest = principal * years * rate / 100.0;
     var year = new Date().getFullYear() + parseInt(years);
 
-    var result = document.getElementById("result").innerHTML =
-        'If you deposit <span class="hitext">' + principal + '</span>,<br/>'
-        + 'at an interest rate of <span class="hitext">' + rate + '</span>%.<br/>'
-        + 'You will receive an amount of <span class="hitext"' + interest + '</span>,<br/>'
-        + 'in the year <span class="hitext"' + year + '</span><br/>';
+    // generate the text presenting the results of the computation.
+    var out = 'If you deposit <span class="hitext">' + principal + '</span>,<br/>'
+        + 'at an interest rate of <span class="hitext">' + rate + '%</span>.<br/>'
+        + 'You will receive an amount of <span class="hitext">' + interest + '</span>,<br/>'
+        + 'in the year <span class="hitext">' + year + '</span><br/>';
+    document.getElementById("result").innerHTML = out;
+        
 }
 
 function updateRate()
 {
+    // updates the display value of interest rate whenever the range onchanged
+    // event is triggered.
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval + "%";
 }
